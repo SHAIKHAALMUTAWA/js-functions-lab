7484784f8f22b419d0e6570a9168ea7c8627d59a
shaikhaalmutawa <shaikha23798@gmail.com>
2024-09-29 10:40:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+node.js
+const movie = ['Barbie', 'Interstellar', 'Get Out'];
+``
+const firstMovie = movies[0];
+// firstMovie is 'Barbie'
+console .log=(movies.length)
+
+const movies  = ['Barbie', 'Interstellar', 'Get Out'];
+ 
+console.log('----------------');
+ const firstbook = book[0];
+const book = ['the shining','pride','prejudic'];
+console .log=(book.length)
+
+// books is ['the shining','pride,'prejudic'];
+const lastbookAt = book.at(-1);  
+// lastbookAt is ''*/
+
+
+constbooks  = []
+console.log(books)
+constbooks  = ['the shining','pride','prejudic'];
+console .log=(book.length)
+
+let books  = [];
+books.push('The Shining');
+books('Pridejudice');
+console.log(books[1]); // Outputs: 'Pridejudice'
+books[1] = 'Dune';
+let i = 0; i < books.length; i;   console.log(books[i]); // Outputs: 'ining', 'Dune'
+
